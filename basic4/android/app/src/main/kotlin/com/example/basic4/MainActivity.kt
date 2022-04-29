02217d4f0db0ccf868928d646f392c92a9e68c9b
Noor Fatima <noorfatima13689@gmail.com>
2022-04-29 21:23:03 +0500

    in progress

---
+++ basic4/android/app/src/main/kotlin/com/example/basic4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basic4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basic4/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Random Words',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Random Words'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
+class RandomWords extends StatefulWidget {
+  const RandomWords({ Key? key }) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
